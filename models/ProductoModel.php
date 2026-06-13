533ed9f2c8f808080023f426642cc55de932acbc
Gabo1308 <gaboleitona@gmail.com>
2026-06-12 18:02:22 -0600

    ProyectoCafeteriaRonroneo

---
+++ models/ProductoModel.php
@@ -0,0 +1,42 @@
+<?php
+class ProductoModel
+{
+    public $enlace;
+    public function __construct()
+    {
+        $this->enlace = new MySqlConnect();
+    }
+
+    public function all()
+    {
+        try {
+            $vSql = "SELECT * FROM productos;";
+            $vResultado = $this->enlace->ExecuteSQL($vSql);
+            return $vResultado;
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+    public function get($id)
+    {
+        try {
+            $vSql = "SELECT * FROM productos WHERE IdProducto=$id;";
+            $vResultado = $this->enlace->ExecuteSQL($vSql);
+            return $vResultado[0];
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+    public function getByCategoria($idCategoria)
+    {
+        try {
+            $vSql = "SELECT * FROM productos WHERE IdCategoria=$idCategoria;";
+            $vResultado = $this->enlace->ExecuteSQL($vSql);
+            return $vResultado;
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+}
