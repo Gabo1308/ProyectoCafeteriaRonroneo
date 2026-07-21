16e4b747904259b6c465dc9598cb889b68565eef
angeloPG07 <angeloperezgonzalez096@gmail.com>
2026-07-20 19:12:54 -0600

    Cambia posicion de los toasts

--- ProyectoCafeteriaRonroneo/src/components/Layout/Layout.jsx
+++ ProyectoCafeteriaRonroneo/src/components/Layout/Layout.jsx
@@ -1,29 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'; 
-import PropTypes from 'prop-types'; 
-import { Container } from '@mui/material'; 
-import Header from './HeaderRonroneo'; 
-import { Footer } from './Footer'; 
-import { Toaster } from 'react-hot-toast';
- 
-Layout.propTypes = { children: PropTypes.node.isRequired }; 
- 
-export function Layout({ children }) { 
-  return ( 
+import React from "react";
+import PropTypes from "prop-types";
+import { Container } from "@mui/material";
+import Header from "./HeaderRonroneo";
+import { Footer } from "./Footer";
+import { Toaster } from "react-hot-toast";
+
+Layout.propTypes = { children: PropTypes.node.isRequired };
+
+export function Layout({ children }) {
+  return (
     <Container
       disableGutters
       maxWidth={false}
-      sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}
+      sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
     >
-      <Header /> 
-      <Container 
-        maxWidth="xl" 
-        sx={{ py: 2, flexGrow: 1 }}
-      > 
-      <Toaster position='bottom-right' />
-        {children} 
-      </Container> 
-      <Footer /> 
+      <Header />
+      <Container maxWidth="xl" sx={{ py: 2, flexGrow: 1 }}>
+        <Toaster position="top-center" />
+        {children}
+      </Container>
+      <Footer />
     </Container>
-  ); 
-} 
+  );
+}
