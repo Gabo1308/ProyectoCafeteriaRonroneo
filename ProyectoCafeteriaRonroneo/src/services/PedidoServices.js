4d1695a35079b305e818c0e1abee1446f405acca
Gabo1308 <gaboleitona@gmail.com>
2026-08-05 13:08:14 -0600

    PedidoService-Actualizado

--- ProyectoCafeteriaRonroneo/src/services/PedidoServices.js
+++ ProyectoCafeteriaRonroneo/src/services/PedidoServices.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BASE_URL + 'pedido';
+
 class PedidoServices {
   crearPedido(pedido) {
     return axios.post(BASE_URL, JSON.stringify(pedido));
   }
-  getPedidosByUsuario(IdUsuario) {
-    return axios.get(BASE_URL + '/getByUsuario/' + IdUsuario);
+  getPedido(idPedido) {
+    return axios.get(BASE_URL + '/get/' + idPedido);
+  }
+  getHistorialCliente(idUsuario, filtros = {}) {
+    return axios.get(BASE_URL + '/getByCliente/' + idUsuario, { params: filtros });
+  }
+  getHistorialTodos(filtros = {}) {
+    return axios.get(BASE_URL + '/getAll', { params: filtros });
+  }
+  getClientes() {
+    return axios.get(BASE_URL + '/getClientes');
   }
 }
+
 export default new PedidoServices();
